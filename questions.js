0e7325628d3117a04bec7e2bc08b01f6af4202fd
Micka <baguet.mickael@gmail.com>
2019-04-16 14:43:46 +0200

    premier commit

--- questions.js
+++ questions.js
@@ -1,55 +1,56 @@
 var languagesArrayCreation = function () {
 
-  return 'A compléter';
+  return languages = ["Html","CSS","Java","PHP"];
 }
 
 var numbersArrayCreation = function () {
 
-    return 'A compléter';
+    return numbers = [0,1,2,3,4,5];
 }
 
 var ElementReplacement = function (languages) {
-
-  return 'A compléter';
+  // languages[2] = 'Javascript';
+  languages.splice(2, 1, "Javascript");
+  return languages;
 }
 
 var AddElementToLanguagesArray = function (languages) {
-
-  return 'A compléter';
+  languages.push("Ruby", "Python");
+  return languages;
 }
 
 var AddElementToNumbersArray = function (numbers) {
-
-  return 'A compléter';
+  numbers.unshift(-2, -1);
+  return numbers;
 }
 
 var deleteArrayFirstElement = function (languages) {
-
-  return 'A compléter';
+  languages.shift();
+  return languages;
 }
 
 var deleteArrayLastElement = function (languages) {
-
-  return 'A compléter';
+  languages.pop();
+  return languages;
 }
 
 var stringToArray = function (socialMediaInString) {
-
-  return 'A compléter';
+  var socialMedia = socialMediaInString.split(',');
+  return socialMedia;
 }
 
 var arrayToString = function (languages) {
-
-  return 'A compléter';
+  var languagesInString = languages.join();
+  // languages.join();
+  return languagesInString;
 }
 
 var arraySort = function (socialMedia) {
-
-  return 'A compléter';
+  socialMedia.sort();
+  return socialMedia;
 }
 
 var arrayInvert = function (languages){
-
-
-  return 'A compléter';
+  languages.reverse();
+  return languages;
 }
